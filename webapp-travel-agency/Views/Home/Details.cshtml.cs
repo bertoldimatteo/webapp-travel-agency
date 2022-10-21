b8d14ff202b13a2e4b6886d8e40479f32d12db20
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-21 09:33:06 +0200

    aggiunta view dettaglio

---
+++ webapp-travel-agency/Views/Home/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp_travel_agency.Views.Home
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
