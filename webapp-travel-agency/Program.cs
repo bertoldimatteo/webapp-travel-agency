62ed9e8813912f0397f9b1892f3a64b9939e1273
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-21 13:01:08 +0200

    hotfix 2

--- webapp-travel-agency/Program.cs
+++ webapp-travel-agency/Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Travel}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
